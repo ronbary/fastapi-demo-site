751643b90ada3ad9027f181bf6e1791401923e87
ron bar yosef <ronibary@gmail.com>
2022-07-06 17:16:33 +0300

    broke one of the tests in purpose !!! to check no CD to heroku

--- app/main.py
+++ app/main.py
@@ -98,7 +98,7 @@ app.include_router(vote.router)
 
 @app.get("/",status_code=status.HTTP_200_OK)
 def root():
-    return {"message": "Hello from  cool!"}
+    return {"message": "Hello from ron broken test cool!"}
     # return {"message": "Hello from roni ...  this is my FastAPI site , cool!"}
 
 
